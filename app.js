f662c5291db0e2816b1aeb879ccacf392266a6e7
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2021-09-23 00:57:03 -0400

    added Player movement

--- app.js
+++ app.js
@@ -17,7 +17,22 @@ var Player = function(id) {
         x: 250, 
         y: 250,
         id:id,
-        number:"" + Math.floor(10 * Math.random())
+        number:"" + Math.floor(10 * Math.random()),
+        pressingRight:false,
+        pressingLeft:false,
+        pressingUp:false,
+        pressingDown:false,
+        maxSpd:5,
+    }
+    self.updatePosition = function() {
+        if(self.pressingRight)
+            self.x += self.maxSpd;
+        if(self.pressingLeft)
+            self.x -= self.maxSpd;
+        if(self.pressingUp)
+            self.y -= self.maxSpd;
+        if(self.pressingDown)
+            self.y += self.maxSpd;
     }
     return self;
 }
@@ -33,13 +48,22 @@ io.sockets.on('connection', function(socket){
         delete SOCKET_LIST[socket.id];
         delete PLAYER_LIST[socket.id];
     });
+	socket.on('keyPress',function(data){
+		if(data.inputId === 'left')
+			player.pressingLeft = data.state;
+		else if(data.inputId === 'right')
+			player.pressingRight = data.state;
+		else if(data.inputId === 'up')
+			player.pressingUp = data.state;
+		else if(data.inputId === 'down')
+			player.pressingDown = data.state;
+	});
 });
 setInterval(function() {
     var pack = [];
     for(var i in PLAYER_LIST) {
         var player = PLAYER_LIST[i];
-        player.x++;
-        player.y++;
+        player.updatePosition();
         pack.push({
             x: player.x,
             y: player.y,
